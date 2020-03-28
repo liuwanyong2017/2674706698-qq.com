c1d14bec84f7a2c446bbc5ed103e0926c4cf896d
liuwanyong2017 <2674706698@qq.com>
2020-03-28 16:10:46 +0800

    队列-击鼓传花的问题的解决！

--- queue.js
+++ queue.js
@@ -6,7 +6,7 @@ class Queue {
 
     //尾部新增
     enqueue(...lists) {
-        lists.map(list=>this.lists.push(list));
+        lists.map(list => this.lists.push(list));
     }
 
     //首部移除
@@ -29,18 +29,72 @@ class Queue {
     size() {
         return this.lists.length;
     }
+
     //转成字符串：
-    toString(){
+    toString() {
         return this.lists.reduce(
-            (a,b)=>a+ (a? ' <- ':'')+(''+b),''
-        )
+            (a, b) => a + (a ? " <- " : "") + ("" + b), ""
+        );
     }
 }
 
 
-const queue = new Queue([1,2,3])
-console.log(queue.toString(),queue.front(),);
-queue.enqueue(6)
-queue.enqueue(7,8,9)
+const queue = new Queue([1, 2, 3]);
+console.log(queue.toString(), queue.front(),);
+queue.enqueue(6);
+queue.enqueue(7, 8, 9);
 
-console.log(queue.dequeue(),queue);
+console.log(queue.dequeue(), queue);
+
+//击鼓传花题目：
+
+const q = new Queue(["a", "b", "c", "d", "e"]);
+
+let timer = 0;
+//第一次淘汰：
+while (timer < 5) {
+    const dequeue = q.dequeue();
+    timer !== 4 && q.enqueue(dequeue);
+    timer === 4 && console.log(dequeue, q);
+    timer++;
+}
+
+timer = 0;
+//第2次淘汰：
+while (timer < 5) {
+    const dequeue = q.dequeue();
+    timer !== 4 && q.enqueue(dequeue);
+    timer === 4 && console.log(dequeue, q);
+    timer++;
+}
+
+timer = 0;
+//第3次淘汰：
+while (timer < 5) {
+    const dequeue = q.dequeue();
+    timer !== 4 && q.enqueue(dequeue);
+    timer === 4 && console.log(dequeue, q);
+    timer++;
+}
+
+timer = 0;
+//第4次淘汰：
+while (timer < 5) {
+    const dequeue = q.dequeue();
+    timer !== 4 && q.enqueue(dequeue);
+    timer === 4 && console.log(dequeue, q);
+    timer++;
+}
+
+function drummingFlowers(arr, loopNum) {
+    const q = new Queue(arr);
+    let timer = 1;
+    while (q.size() > 1) {
+        const dequeue = q.dequeue();
+        timer % loopNum !== 0 && q.enqueue(dequeue);
+        timer++;
+    }
+    return q.front();
+}
+
+console.log(drummingFlowers(["a", "b", "c", "d", "e"], 6));
