eabba0d91ec9b5372e3b58580281b8d2a1e5d786
liuwanyong2017 <2674706698@qq.com>
2020-03-28 11:54:50 +0800

    修改pop的方法的返回值，忘了return 。同时用栈实现了10进制转2进制的方法了。

--- stack.js
+++ stack.js
@@ -4,45 +4,75 @@
 //基于数组：
 class Stack {
     constructor(arr) {
-        this.lists = arr
+        this.lists = arr;
     }
+
     //压入栈
-    push(list){
-        this.lists.push(list)
+    push(list) {
+        this.lists.push(list);
     }
+
     // 取出栈顶元素
-    pop(){
-        this.lists.pop()
+    pop() {
+        return this.lists.pop();
     }
+
     // 查看栈顶元素
-    peek(){
-        return this.lists[this.lists.length -1]
+    peek() {
+        return this.lists[this.lists.length - 1];
     }
+
     // 判断栈是否为空
-    isEmpty(){
-        return this.lists.length === 0
+    isEmpty() {
+        return this.lists.length === 0;
     }
+
     // 获取栈中元素个数
-    size(){
-        return this.lists.length
+    size() {
+        return this.lists.length;
     }
+
     //toString
-    toString(){
+    toString() {
         return this.lists.reduce(
-            (a,b)=>
-                a+ (a?'->':'') + (''+b)
-            ,''
-        )
+            (a, b) =>
+                a + (a ? "->" : "") + ("" + b)
+            , ""
+        );
     }
 }
-const initArr = [6,5,4]
+
+const initArr = [6, 5, 4];
 const s = new Stack(initArr);
 
 console.log(s);
 console.log(s.peek());
-s.pop()
-s.push(3)
+s.pop();
+s.push(3);
 console.log(s);
 console.log(s.size());
 console.log(s.toString());
 console.log(s.isEmpty());
+
+//十进制转二进制：num %2 的余数从后往前拼接，num每次都重新赋值 Math.floor(num/2)
+
+function dec2bin(num) {
+    const s = new Stack([]);
+    while (num > 0) {
+        s.push(num % 2);
+        num = Math.floor(num / 2);
+    }
+    let res = "";
+    while (!s.isEmpty()) {
+        res += s.pop();
+    }
+    return res;
+}
+
+console.log(dec2bin(100));
+console.log(dec2bin(1));
+console.log(dec2bin(2));
+console.log(dec2bin(10));
+
+
+
