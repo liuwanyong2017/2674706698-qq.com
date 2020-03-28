c29b241997c32d8339fbca021a949db49ea5dd62
liuwanyong2017 <2674706698@qq.com>
2020-03-28 20:23:42 +0800

    优先级队列的增加，实现，难点是自己尝试用2分法做了一下！2分法牛皮！

--- queue.js
+++ queue.js
@@ -39,12 +39,12 @@ class Queue {
 }
 
 
-const queue = new Queue([1, 2, 3]);
-console.log(queue.toString(), queue.front(),);
-queue.enqueue(6);
-queue.enqueue(7, 8, 9);
-
-console.log(queue.dequeue(), queue);
+// const queue = new Queue([1, 2, 3]);
+// console.log(queue.toString(), queue.front(),);
+// queue.enqueue(6);
+// queue.enqueue(7, 8, 9);
+//
+// console.log(queue.dequeue(), queue);
 
 //击鼓传花题目：
 
@@ -98,3 +98,91 @@ function drummingFlowers(arr, loopNum) {
 }
 
 console.log(drummingFlowers(["a", "b", "c", "d", "e"], 6));
+
+//优先级队列的实现：
+//默认的传入数据为{data:xxx,priority:1}
+//priority的值不可重复，递增式的数字类型！
+//初始默认的arr参数，顺序遵循priority的order。
+class PriorityQueue extends Queue {
+    enqueue(...lists) {
+        lists.map(
+            list => {
+                if (
+                    this.lists.length === 0 ||
+                    this.lists[this.lists.length - 1].priority > list.priority
+                ) return this.lists.push(list);
+
+                if (this.lists.length < 6) {
+                    let res;
+                    for (let i = 0; i < this.lists.length; i++) {
+                        if (this.lists[i].priority < list.priority) {
+                            this.lists.splice(i, 0, list);
+                            res = true;
+                            break;
+                        }
+                    }
+                } else {
+                    let getIndex = (length) =>
+                        // length % 2 === 0 ? [length / 2 - 1, length / 2] :
+                        Math.round(length / 2) - 1
+                    ;
+                    let length = this.lists.length;
+                    let index = getIndex(length);
+                    let max, min;
+                    while (index) {
+                        if (min && max && min + 1 === max) {
+                            this.lists.splice(max - 1, 0, list);
+                            return index = null;
+                        }
+
+                        const {priority} = this.lists[index];
+                        console.log("indss:", priority, index, "dd", min, max);
+                        if (priority > list.priority) {
+                            min = min && min > index + 1 ? min : index + 1;
+                            length = min + this.lists.length;
+                            index = getIndex(length);
+                            // console.log('in', index,'pri:', priority);
+                        } else {
+                            const {priority} = this.lists[index - 1];
+                            // console.log('in2222', index,'pri:', priority);
+
+                            if (priority > list.priority) {
+                                this.lists.splice(index, 0, list);
+                                index = null;
+                            } else {
+                                max = max && max < index ? max : index;
+                                length = max + 1;
+                                index = getIndex(length);
+                            }
+                            // console.log('in2', index,'pri:', priority);
+                        }
+                    }
+                }
+            }
+        );
+    }
+}
+
+
+const pq = new PriorityQueue(
+    [
+        {data: 1, priority: 300},
+        {data: 1, priority: 200},
+        {data: 1, priority: 100},
+        {data: 1, priority: 50},
+        {data: 1, priority: 25},
+        {data: 1, priority: 12},
+        {data: 1, priority: 6}
+    ]
+);
+
+pq.enqueue({data: 2, priority: 40});
+
+pq.enqueue(
+    {data: 2, priority: 30},
+    {data: 2, priority: 400},
+    {data: 2, priority: 7},
+    {data: 2, priority: 150}
+);
+
+console.log(pq, 0);
