54a26f050134cfb20b13ebe0f13e9afac02dcc1b
liuwanyong2017 <2674706698@qq.com>
2020-03-29 13:49:45 +0800

    fix append方法，忘了length递增了。同时实现了insert的方法

--- linked-list.js
+++ linked-list.js
@@ -8,7 +8,6 @@ class LinkedList {
     append(content) {
         const list = new List(content);
         if (this.length === 0) {
-            this.length += 1;
             return this.header = list;
         }
         let current = this.header;
@@ -16,6 +15,32 @@ class LinkedList {
             current = current.next;
         }
         current.next = list;
+        this.length += 1;
+    }
+
+    insert(content, index) {
+        if (index < 0 || index > this.length - 1) {
+            throw new Error("index参数不合理");
+        }
+        const list = new List(content);
+        let oldList = this.header;
+        if (index === 0) {
+            oldList && (list.next = oldList);
+            this.header = list;
+        } else {
+            let i = 0;
+            let last;
+            while (i < index) {
+                if (i === index - 1) {
+                    last = oldList;
+                }
+                oldList = oldList.next;
+                i++;
+            }
+            last.next = list;
+            list.next = oldList;
+        }
+        this.length++;
     }
 
     size() {
@@ -27,7 +52,13 @@ class LinkedList {
     }
 
     toString() {
-
+        let current = this.header;
+        let res = "";
+        while (current) {
+            res += (res ? " -> " : "") + current.content;
+            current = current.next;
+        }
+        return res;
     }
 }
 
@@ -42,4 +73,10 @@ const list = new List(1);
 const linkedList = new LinkedList(list);
 console.log(linkedList);
 linkedList.append(30);
-console.log(linkedList);
+linkedList.insert("2", 1);
+linkedList.insert("0", 0);
+linkedList.insert("6", 3);
+linkedList.insert("20", 4);
+linkedList.insert("89", 5);
+
+console.log(linkedList, linkedList.toString(), 888);
