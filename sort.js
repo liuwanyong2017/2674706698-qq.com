ed5df36c5077b4f7acd222d960c5d7f295ad4a66
liuwanyong2017 <2674706698@qq.com>
2020-03-31 00:11:24 +0800

    简单的排序算法，优化，通过赋值换位，加遍历的状态管理，达到了我所认为的最优了。我很强！

--- sort.js
+++ sort.js
@@ -9,10 +9,12 @@ class Sort {
         this.data = [...data];
         this.min = [];
         this.max = [];
+        this.left = 0;
+        this.right = data.length - 1;
     }
 
     loop() {
-        const {data} = this
+        const {data} = this;
         let min = data[0], max = data[0];
         let minIndex = 0, maxIndex = 0;
         data.map(
@@ -31,17 +33,66 @@ class Sort {
         this.max.unshift(max);
         const larger = minIndex > maxIndex ? minIndex : maxIndex;
         const small = minIndex < maxIndex ? minIndex : maxIndex;
-        data.splice(larger,1);  //这里的性能有损耗，最好的方法是用双链表
-        data.splice(small,1);
-        while(data.length>1){
-            this.loop()
+        data.splice(larger, 1);  //这里的性能有损耗，最好的方法是用双链表
+        data.splice(small, 1);
+        while (data.length > 1) {
+            this.loop();
         }
-        if (data.length === 1){
-            this.min.push(data[0])
+        if (data.length === 1) {
+            this.min.push(data[0]);
+        }
+    }
+
+    loop1() {
+        const {data} = this;
+        let min = data[this.left], max = data[this.left];
+        let minIndex = [this.left], maxIndex = [this.left];
+        for (let i = this.left; i <= this.right; i++) {
+            if (data[i] > max) {
+                max = data[i];
+                maxIndex = [i];
+            } else if (data[i] === max && i !== this.left) {
+                maxIndex.push(i);
+            }
+            if (data[i] < min) {
+                min = data[i];
+                minIndex = [i];
+            } else if (data[i] === min && i !== this.left) {
+                minIndex.push(i);
+            }
+            // console.log(minIndex,min,max,maxIndex)
+        }
+        if (max === min) return;
+        minIndex.map(
+            ind => {
+                if (ind !== this.left) {
+                    const leftVal = data[this.left];
+                    const indVal = data[ind];
+                    data[this.left] = indVal;
+                    data[ind] = leftVal;
+                }
+                this.left += 1;
+            }
+        );
+        maxIndex.map(
+            ind => {
+                if (ind !== this.right) {
+                    const rightVal = data[this.right];
+                    const indVal = data[ind];
+                    data[this.right] = indVal;
+                    data[ind] = rightVal;
+                }
+                this.right -= 1;
+            }
+        );
+        while(this.right-this.left>1){
+            this.loop1()
         }
     }
 }
 
-const sort = new Sort(arr)
-sort.loop()
-console.log(arr,sort);
+const sort = new Sort(arr);
+// sort.loop();
+// console.log(arr, sort);
+sort.loop1()
+console.log(sort.data)
