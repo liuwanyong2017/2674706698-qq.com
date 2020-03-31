0e11f7de7cc31bb489415f718c3e8c68f5743fdc
liuwanyong2017 <2674706698@qq.com>
2020-03-31 18:00:06 +0800

    并集的实现

--- set.js
+++ set.js
@@ -40,8 +40,48 @@ set.add(2);
 set.add("b");
 set.add("3");
 console.log(
-    set, set.has('2'),
+    set, set.has("2"),
     set.remove(3),
     set.size(),
     set.values()
 );
+
+//并集的操作 a U b , 元素存于a or 元素存于b:
+
+
+class Union extends Set {
+    constructor(arr,initData) {
+        super(initData);
+        if (!Array.isArray(arr) || arr.find(
+            item => !(item instanceof Set)
+        )) {
+            throw new Error("Union 必须要接收一个Set类组成的数组参数！");
+        }
+        this.init(arr);
+    }
+
+    init(arr) {
+        arr.map(
+            list => {
+                if (list instanceof Set) {
+                    const isEmpty = this.size() === 0;
+                    list.values().map(
+                        val => isEmpty ? this.add(val) :
+                            !this.has(val) && this.add(val)
+                    );
+                }
+            }
+        );
+    }
+}
+
+const set1 = new Set({
+    a:'a',b:'b',c:'c',4:4,6:'6'
+})
+const set2 = new Set({
+    b:'b',c:'c',4:'4',7:'7',l:'l'
+})
+const union = new Union([set1,set2],{7:7})
+console.log(union);
+
+
