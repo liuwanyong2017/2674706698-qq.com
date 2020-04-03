edc2665ee9bfbbe235f12107b9847090816b7213
liuwanyong2017 <2674706698@qq.com>
2020-04-04 00:51:00 +0800

    实现先序遍历，中序遍历，记得：递归！

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -38,16 +38,54 @@ class BinarySearchTree {
         }
     }
 
+    //先序遍历
 
+    preOrderTraversalNode(callback) {
+        //先遍历好左节点，再遍历右节点：
+        this._preOrderTraversalNode(this.root, callback);
+    }
+
+    _preOrderTraversalNode(node, callback) {
+        //先遍历好左节点，再遍历右节点：
+        if (node !== null) {
+            callback(node.key, node.data);
+            this._preOrderTraversalNode(node.left, callback);
+            this._preOrderTraversalNode(node.right, callback);
+        }
+    }
+
+    midOrderTraversalNode(callback) {
+        this._midOrderTraversalNode(this.root, callback);
+    }
+
+    _midOrderTraversalNode(node, callback) {
+        if (node) {
+            this._midOrderTraversalNode(node.left, callback);
+            callback(node.key, node.data);
+            this._midOrderTraversalNode(node.right, callback);
+        }
+    }
 }
 
 
-const binarySearchTree = new BinarySearchTree()
-const arr = []
-for(let i =0;i<10;i++){
-    arr.push(Math.floor(Math.random()*100))
+const binarySearchTree = new BinarySearchTree();
+const arr = [];
+for (let i = 0; i < 10; i++) {
+    arr.push(Math.floor(Math.random() * 100));
 }
 arr.map(
-    (num,ind)=>binarySearchTree.insert(num,{id:ind})
-)
+    (num, ind) => binarySearchTree.insert(num, {id: ind})
+);
 console.log(binarySearchTree);
+let arr1 = [], arr2 = [];
+binarySearchTree.preOrderTraversalNode(
+    (key, data) => {
+        arr1.push({key, data});
+    }
+);
+binarySearchTree.midOrderTraversalNode(
+    (key, data) => {
+        arr2.push({key, data});
+    }
+);
+console.log(arr, arr1, arr2);
