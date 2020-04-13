1a7975e1720402853f4309eca08bd484346946e8
liuwanyong2017 <2674706698@qq.com>
2020-04-13 22:55:08 +0800

    图的添加顶点，边的方法实现

--- graph.js
+++ graph.js
@@ -1,14 +1,59 @@
 //图封装：邻接表
 
 
-import {Dictionary} from "./dictionary";
+const {Dictionary} = require("./dictionary");
 
-const dictionary = new Dictionary()
+const dictionary = new Dictionary();
 
 
 class Graph {
-    constructor(dictionary,vertexes=[]) {
-        this.vertexes = vertexes
-        this.edges = dictionary || new Dictionary()
+    constructor(vertexes = []) {
+        this.vertexes = vertexes;
+        let obj = {};
+        if (vertexes.length) {
+            vertexes.map(
+                key => obj[key] = []
+            );
+        }
+        this.edges = new Dictionary(obj);
+    }
+
+    hasVertex(v) {
+        return this.vertexes.indexOf(v) >= 0;
+    }
+
+    addVertex(v) {
+        if (!this.hasVertex(v)) return false;
+        this.vertexes.push(v);
+        this.edges[v] = [];
+        return true;
+    }
+
+    addEdge(v, v1, unidirectional) {
+        if (!this.hasVertex(v) || !this.hasVertex(v1)) return false;
+        const edges1 = this.edges.get(v), edges2 = this.edges.get(v1);
+        if (edges1.indexOf(v1) < 0) {
+            edges1.push(v1);
+        }
+        if (!unidirectional && edges2.indexOf(v) < 0) {
+            edges2.push(v);
+        }
+        // console.log(edges2,edges1);
+        return true;
     }
 }
+
+const graph = new Graph(["a", "b", "c", "d", "e", "f", "g", "h", "i"]);
+
+graph.addEdge("a", "b");
+graph.addEdge("a", "c");
+graph.addEdge("a", "d");
+graph.addEdge("c", "d");
+graph.addEdge("c", "g");
+graph.addEdge("d", "g");
+graph.addEdge("d", "h");
+graph.addEdge("b", "e");
+graph.addEdge("b", "f");
+graph.addEdge("e", "i");
+
+console.log(graph);
