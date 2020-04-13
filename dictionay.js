a9c0de7ec5b22ef8ce6bca74f3b7055fe3643c1f
liuwanyong2017 <2674706698@qq.com>
2020-04-13 12:23:07 +0800

    封装字典

---
+++ dictionay.js
@@ -0,0 +1,41 @@
+//字典
+
+
+class Dictionay {
+    constructor(items) {
+        this.items = items;
+    }
+
+    has(key) {
+        return this.items.hasOwnProperty(key);
+    }
+
+    set(key, val) {
+        this.items[key] = val;
+    }
+
+    remove(key) {
+        if (!this.has(key)) return false;
+        delete this.items[key];
+        return true;
+    }
+
+    get(key) {
+        if (!this.has(key)) return undefined;
+        return this.items[key];
+    }
+
+    get keys() {
+        return this.items && Object.keys(this.items);
+    }
+
+    get values() {
+        return this.items && Object.values(this.items);
+    }
+    get size() {
+        return this.keys.length;
+    }
+    clear() {
+        return this.items = {};
+    }
+}
