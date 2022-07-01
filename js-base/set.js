c9c993078808d472bf270d5000116cf87ccd4984
liuwanyong2017 <2674706698@qq.com>
2022-07-01 17:29:19 +0800

    set数据结构:add值时,去重的判断为===全等于,例外是对NaN的值判断是相等的

--- js-base/set.js
+++ js-base/set.js
@@ -2,7 +2,7 @@
  * @Author: liuwanyong2017 2674706698@qq.com
  * @Date: 2022-07-01 16:54:19
  * @LastEditors: liuwanyong2017 2674706698@qq.com
- * @LastEditTime: 2022-07-01 17:18:38
+ * @LastEditTime: 2022-07-01 17:26:35
  * @FilePath: /dataStructure/js-base/set.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,5 +21,20 @@
   // 4
   // Set(3) { 2, 3, 5 }
   console.log(new Set('sbsbdff'))
-//   Set(4) { 's', 'b', 'd', 'f' }
+  //   Set(4) { 's', 'b', 'd', 'f' }
+}
+{
+  //add 值的时候，进行的是===判断去重，对值的判断，
+  //除了NaN判断为自己等于自己，其他的以===判断
+  let s = new Set()
+  s.add(NaN)
+  console.log(s.size)
+  s.add(NaN)
+  console.log(s.size)
+  s.add({})
+  s.add({})
+  console.log(s);
+//   1
+// 1
+// Set(3) { NaN, {}, {} }
 }
