b8419893db174804131f4e2292b46d8bf0628fc0
liuwanyong2017 <2674706698@qq.com>
2022-07-01 18:19:44 +0800

    set :Array.from转化为数组

--- js-base/set.js
+++ js-base/set.js
@@ -2,7 +2,7 @@
  * @Author: liuwanyong2017 2674706698@qq.com
  * @Date: 2022-07-01 16:54:19
  * @LastEditors: liuwanyong2017 2674706698@qq.com
- * @LastEditTime: 2022-07-01 17:34:27
+ * @LastEditTime: 2022-07-01 18:17:59
  * @FilePath: /dataStructure/js-base/set.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -51,4 +51,9 @@
   // true
   // true false
   // Set(0) {}
+}
+{
+//tobe Array
+Array.from(new Set([1,2,2,4]))
+//[1,2,4]
 }
