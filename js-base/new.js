89b6cd204ed9dd7f0c4893c2c7e6f2a20cf7bd5e
liuwanyong2017 <2674706698@qq.com>
2022-07-01 16:32:34 +0800

    原型链的终点是Object.prototype.__proto__,值为null

--- js-base/new.js
+++ js-base/new.js
@@ -2,7 +2,7 @@
  * @Author: liuwanyong2017 2674706698@qq.com
  * @Date: 2022-07-01 01:19:40
  * @LastEditors: liuwanyong2017 2674706698@qq.com
- * @LastEditTime: 2022-07-01 11:47:38
+ * @LastEditTime: 2022-07-01 16:30:27
  * @FilePath: /dataStructure/js-base/new.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,4 +29,12 @@ function privateNew(f, ...args) {
   f.constructor === F
   Object.getPrototypeOf(f) === proto1
   //这就是关系了，虽然修改了F的prototype。但是原本的f的原型被f保留了下来了。
+}
+{
+  //原型链的终点：可以通俗的说，确实是万物都是对象，所以原型链的根源会是Object上面，
+  console.log(Object.prototype)
+  console.log(Object.prototype.__proto__);
+//   [Object: null prototype] {}
+// null
+//根源就是 null
 }
